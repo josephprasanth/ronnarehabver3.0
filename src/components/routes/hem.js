2c33d9bcfa785b05cd4872e169c0565c6cde0fb9
josephprasanth <prasanthjoseph_77@yahoo.com>
2020-01-04 08:48:12 +0100

    Created new features section

--- src/components/routes/hem.js
+++ src/components/routes/hem.js
@@ -2,21 +2,44 @@ import React, { Component } from 'react';
 import './Hem.css';
 
 class Hem extends Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (
             <>
                 <div className="hero-section">
                     <div className="top-heading">
-                    <h1>Välkommen till Ronna Rehab</h1>
+                        <h1>Välkommen till Ronna Rehab</h1>
                     </div>
                     <div className="bottom-heading">
-                    <h3>Den lilla mottagning med störa hjärtat</h3>
+                        <h4>Den lilla mottagning med störa hjärtat</h4>
+                    </div>
+                </div>
+                <div className="features-section">
+                    <div className="columns-wrapper">
+                        <div className="column">
+                            {/*Icon goes here*/}
+
+                            <p>Patientens Fokus</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <div className="column">
+                            {/*Icon goes here*/}
+
+                            <p>ArbetsGlädje</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <div className="column">
+                            {/*Icon goes here*/}
+
+                            <p>Kunskap</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+
                     </div>
                 </div>
             </>
         );;
     }
 }
- 
+
 export default Hem;
